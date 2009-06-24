f56166d13a28fa77488c5976a6c28dab6b4c655c
Stefano Maria Iacus <stefano.iacus@unimi.it>
2009-06-24 00:00:00 +0000

    version 2.0.7

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@
 {
 # cat("Companion package to the book:\n")
 # cat("Simulation and Inference for Stochastic Differential Equations With R Examples, Springer NY, (2008)\n")
- 
+  cat("\nTo check the errata corrige of the book, type vignette(\"sde.errata\")\n")
  library.dynam("sde", pkgname, libname) 
 }
